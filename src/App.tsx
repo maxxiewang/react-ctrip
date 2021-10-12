bd29899d08764f00f4bc8025439753dc0e2fb58e
wangfei_pro <51104860@163.com>
2021-10-12 22:55:58 +0800

    init success, start header-part

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
-// import './App.css';
 import styles from './App.module.css'
+import { Layout , Typography, Input } from 'antd'
 
 function App() {
   return (
     <div className={styles.App}>
-      <header className={styles['App-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles['App-link']}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Layout.Header>
+          <img src={logo} alt="" className={styles['App-logo']}/>
+          {/* 这个level={3} 是React提前定义好的，直接使用就行 */}
+          <Typography.Title level={3}>CyberTrip</Typography.Title>
+          <Input.Search placeholder="请输入关键字"></Input.Search>
+        </Layout.Header>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'antd/dist/antd.css'; // 全局引用antd
 
 ReactDOM.render(
   <React.StrictMode>
