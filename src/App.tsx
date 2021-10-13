69ee0e256f0c5e2261e32047a13b773ccb6ca333
wangfei_pro <51104860@163.com>
2021-10-13 21:11:09 +0800

    组件化拆分header与footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import styles from './App.module.css'
-import { Layout , Typography, Input } from 'antd'
+import styles from "./App.module.css";
+import {Header, Footer} from './components'
 
 function App() {
   return (
     <div className={styles.App}>
-      <div>
-        <Layout.Header>
-          <img src={logo} alt="" className={styles['App-logo']}/>
-          {/* 这个level={3} 是React提前定义好的，直接使用就行 */}
-          <Typography.Title level={3}>CyberTrip</Typography.Title>
-          <Input.Search placeholder="请输入关键字"></Input.Search>
-        </Layout.Header>
-      </div>
+      <Header/>
+      <Footer/>      
     </div>
   );
 }
