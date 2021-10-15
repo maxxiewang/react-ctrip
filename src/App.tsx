a478c213387b6dd8942386e88ecad834b810cb46
wangfei_pro <51104860@163.com>
2021-10-15 21:47:38 +0800

    1015commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import styles from "./App.module.css";
 import {Header, Footer} from './components'
+import {Row,Col} from 'antd'
 
 function App() {
   return (
     <div className={styles.App}>
       <Header/>
+      {/* 页面内容 */}
+      <div className={styles['page-content']}>
+      <Row style={{marginTop:10}}>
+        <Col span={6}>
+          <div style={{background:"red"}}>多重菜单</div>
+        </Col>
+        <Col span={18}>
+          <div style={{background:"yellow"}}>轮播图</div>
+        </Col>
+      </Row>
+      </div>
       <Footer/>      
     </div>
   );
