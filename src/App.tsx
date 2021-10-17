6cf3a680e7c02cce2262997c960125bd681a6e1d
wangfei_pro <51104860@163.com>
2021-10-17 13:09:53 +0800

    侧边栏与轮播图

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from "./App.module.css";
-import {Header, Footer} from './components'
+import {Header, Footer, Carousel, SideMenu} from './components'
 import {Row,Col} from 'antd'
 
 function App() {
@@ -11,10 +11,10 @@ function App() {
       <div className={styles['page-content']}>
       <Row style={{marginTop:10}}>
         <Col span={6}>
-          <div style={{background:"red"}}>多重菜单</div>
+          <SideMenu />
         </Col>
         <Col span={18}>
-          <div style={{background:"yellow"}}>轮播图</div>
+          <Carousel />
         </Col>
       </Row>
       </div>
